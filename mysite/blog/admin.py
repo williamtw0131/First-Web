533b2446f44e9d151c97a5d747a558e35dcb015f
williamfang <ie4t21@gmail.com>
2018-11-21 20:07:22 +0800

    My Django app, first commit, 2018/11/21

---
+++ mysite/blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+
+# 2018/11/21
+from .models import Post
+# Register your models here.
+admin.site.register(Post)
---
+++ mysite/blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# 2018/11/21
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default = timezone.now)
+    publish_date = models.DateTimeField(blank = True, null = True)
+
+    def publish(self):
+        self.publshed_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
