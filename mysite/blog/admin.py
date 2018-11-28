1a4d2c50e2fec0653157af3e65cc417def6bd88c
William Fang <ie4t21@gmail.com>
2018-11-28 20:28:11 +0800

    Added comment function.

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 
 # 2018/11/21
-from .models import Post
+from .models import Post, Comment
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Comment)
